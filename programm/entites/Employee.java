d9a329d21828dce42877da7b1f8195234bf09706
andrevinicus <102330781+andrevinicus@users.noreply.github.com>
2022-08-12 16:13:59 -0300

    Add files via upload

---
+++ programm/entites/Employee.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.programm.entites;
+
+/**
+ *
+ * @author GCM_02
+ */
+public class Employee {
+    
+    private String name;
+    private Integer hours;
+    private Double valuePerHour;
+    
+    public Employee(){
+    }
+
+    public Employee(String name, Integer hours, Double valuePerHour) {
+        this.name = name;
+        this.hours = hours;
+        this.valuePerHour = valuePerHour;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getHours() {
+        return hours;
+    }
+
+    public void setHours(Integer hours) {
+        this.hours = hours;
+    }
+
+    public Double getValuePerHour() {
+        return valuePerHour;
+    }
+
+    public void setValuePerHour(Double valuePerHour) {
+        this.valuePerHour = valuePerHour;
+    }
+    public double payment(){
+        return hours * valuePerHour; 
+    }
+}
---
+++ programm/entites/OutsurcedEmployee.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.programm.entites;
+
+/**
+ *
+ * @author GCM_02
+ */
+public class OutsurcedEmployee extends Employee {
+
+    private Double additionalCharge;
+
+
+
+    public OutsurcedEmployee(String name, Integer hours, Double valuePerHour, Double additionalCharge) {
+        super(name, hours, valuePerHour);
+        this.additionalCharge = additionalCharge;
+    }
+
+    public OutsurcedEmployee() {
+        super();
+
+    }
+
+    public Double getAdditionalCharge() {
+        return additionalCharge;
+    }
+
+    public void setAdditionalCharge(Double additionalCharge) {
+        this.additionalCharge = additionalCharge;
+    }
+
+    @Override
+    public double payment() {
+        return super.payment() + additionalCharge * 1.1;
+    }
+}
